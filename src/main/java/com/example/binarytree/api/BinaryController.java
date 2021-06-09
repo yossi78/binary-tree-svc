69f72c2a0dd28d3a6b0a174a59fd0d87fbfa117e
yossi78 <yossi78@gmail.com>
2021-06-09 18:59:22 +0300

    fix

--- src/main/java/com/example/binarytree/api/BinaryController.java
+++ src/main/java/com/example/binarytree/api/BinaryController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping(value = "/v1/binay")
+@RequestMapping(value = "/v1/binary")
 public class BinaryController {
 
     private static final Logger logger = LoggerFactory.getLogger(BinaryController.class);
